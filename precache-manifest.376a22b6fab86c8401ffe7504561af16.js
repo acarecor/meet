666914604bc69bee333119b9b1e559e60963fc81
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-08-21 12:33:00 +0200

    Updates

---
+++ precache-manifest.376a22b6fab86c8401ffe7504561af16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6b7b392207f682c5bdb",
+    "url": "/meet.git/static/css/main.24d1ab5e.chunk.css"
+  },
+  {
+    "revision": "b6b7b392207f682c5bdb",
+    "url": "/meet.git/static/js/main.b6b7b392.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/meet.git/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/meet.git/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "d377b21f1123f658f69f",
+    "url": "/meet.git/static/js/runtime~main.d377b21f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meet.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "61da4f0f7a57954776f76570dcc7f7a4",
+    "url": "/meet.git/index.html"
+  }
+];
