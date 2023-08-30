8355fbac0af089e3f82588c5f4471a143033cafb
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-08-30 11:31:54 +0200

    created number of events

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import  userEvent  from '@testing-library/user-event';
+import NumberOfEvents from '../components/NumberOfEvents';
+import mockData from '../mock-data';
+
+
+const event = mockData;
+
+describe('<NumberOfEvents /> component', () => {
+  let NumberOfEventsComponent;
+  beforeEach(() => {
+    NumberOfEventsComponent = render(<NumberOfEvents />);
+  });
+
+  test('render textBox for the number of events', () => {
+    const eventNumber = NumberOfEventsComponent.queryByRole('textbox');
+    expect(eventNumber).toBeInTheDocument();
+  });
+
+  test('default value of the input field (number of events) should be 32', () => {
+    const eventNumber = NumberOfEventsComponent.queryByRole('textbox');
+    expect(eventNumber).toHaveValue('32');
+  });
+
+  test('to ensure that the value in textBox change when user types in it', async () => {
+    const user = userEvent.setup();
+    const eventNumber = NumberOfEventsComponent.queryByRole('textbox');
+    await user.type(eventNumber, '{backspace}{backspace}10');
+    expect(eventNumber).toHaveValue('10');
+  });
+
+});
+
+
+
---
+++ src/components/NumberOfEvents.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useState } from "react";
+    
+
+
+const NumberOfEvents = () => {
+    const [query, setQuery] = useState(32);
+
+    const handleInputChanged =(event) =>{
+     const value = event.target.value;
+     setQuery(value);
+     
+    };
+
+
+  return (
+    <div id="number-of-events">
+      <input 
+        type="text" 
+        value={query} 
+        onChange={handleInputChanged}
+        />
+    </div>
+  );
+};
+
+export default NumberOfEvents;
