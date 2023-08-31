be941e0a96f0d12d58133607d5f25caad8288f82
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-08-31 18:17:01 +0200

    added allLocations prop to citysearch

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import { useState, useEffect } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
-import { getEvents } from './api';
+import { extractLocations, getEvents } from './api';
 
 import './App.css';
 
 const App = () => {
   const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE] = useState([32]);
+  const [allLocations, setAllLocations] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+   }, []);
 
   const fetchData = async () => {
     const allEvents = await getEvents();
     setEvents(allEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
   }
-   useEffect(() => {
-    fetchData();
-   }, []);
   
   return (
     <div className="App">
-      <CitySearch />
+      <CitySearch allLocations={allLocations}/>
       <NumberOfEvents />
       <EventList events={events} />
     </div>
