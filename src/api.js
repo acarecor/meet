ad38a3428a980b8ffb868bdeb8b6149bd112b3fb
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-09-06 14:58:16 +0200

    fixed bugs api

--- src/api.js
+++ src/api.js
@@ -67,7 +67,7 @@ export const getAccessToken = async () => {
   const accessToken = localStorage.getItem("access_token");
   const tokenCheck = accessToken && (await checkToken(accessToken));
 
-  if (!accessToken || tokenCheck.console.error) {
+  if (!accessToken || tokenCheck.error){
     await localStorage.removeItem("access_token");
     const searchParams = new URLSearchParams(window.location.search);
     const code = await searchParams.get("code");
