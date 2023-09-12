4112b09be8b86a1427d7ab7afabdecf204800865
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-09-12 21:44:54 +0200

    added errorAlert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -36,9 +36,17 @@ class Alert extends Component {
 class InfoAlert extends Alert {
     constructor (props) {
         super(props);
-        this.color = 'rgb(0, 0, 255)';
+        this.color = 'rgb(0, 0, 255)'; //blue
         this.bgColor = 'rgb(220,220,255';
     }
 }
 
-export {InfoAlert};
+class ErrorAlert extends Alert {
+    constructor (props) {
+        super(props);
+        this.color = 'rgb(255, 0, 0 )'; //red
+        this.bgColor = 'rgb(255, 235, 235)'; // rosa
+    }
+}
+
+export {InfoAlert, ErrorAlert};
