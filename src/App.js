ae3ec2db81d24ab5acf57c670c318e19a12c501f
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-09-21 10:40:44 +0200

    created event genres chart

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import CityEventsChart from './components/CityEventsChart';
 import NumberOfEvents from './components/NumberOfEvents';
+import EventGenresChart from './components/EventGenresChart';
 import { extractLocations, getEvents } from './api';
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 
@@ -59,9 +60,13 @@ const App = () => {
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}
         />
-      <CityEventsChart
-        allLocations={allLocations}
-        events={events}/>
+      <div className='charts-container'>
+        <EventGenresChart 
+          events={events}/>
+        <CityEventsChart
+          allLocations={allLocations}
+          events={events}/>
+      </div>
        <EventList events={events} />
     </div>
   );
---
+++ src/components/EventGenresChart.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
+
+const EventGenresChart = ({ events }) => {
+  const [data, setData] = useState([]);
+  const genres = ["React", "JavaScript", "Node", "jQuery", "Angular"];
+
+  const getData = () => {
+    const data = genres.map((genre) => {
+      const filteredEvents = events.filter((event) =>
+        event.summary.includes(genre)
+      );
+      return {
+        name: genre,
+        value: filteredEvents.length,
+      };
+    });
+    return data;
+  };
+
+  useEffect(() => {
+    setData(getData());
+    // eslint-disable-next-line
+  }, [`${events}`]);
+
+  return (
+    <ResponsiveContainer width="99%" height={400}>
+        <PieChart >
+          <Pie
+            data={data}
+            dataKey="value"
+            fill="#8884d8"
+            labelLine={false}
+            label
+            outerRadius={130} 
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    );
+  
+};
+
+export default EventGenresChart;
